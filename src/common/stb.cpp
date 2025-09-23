0ad21901dd738f3e6072e60518142067ecf7043d
collecting <collecting@citron-emu.org>
2025-09-23 04:55:23 +0000

    #define STBI_NO_SIMD

--- src/common/stb.cpp
+++ src/common/stb.cpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright 2023 yuzu Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#define STBI_NO_SIMD
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
