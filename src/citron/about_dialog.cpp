cc7b3d506ce436dd64ff80fb3ba6da397fa7b795
Zephyron <zephyron@citron-emu.org>
2025-10-11 13:51:57 +1000

    chore: Version bump to 0.8.0
    Signed-off-by: Zephyron <zephyron@citron-emu.org>

--- src/citron/about_dialog.cpp
+++ src/citron/about_dialog.cpp
@@ -9,7 +9,7 @@
 
 AboutDialog::AboutDialog(QWidget* parent)
     : QDialog(parent), ui{std::make_unique<Ui::AboutDialog>()} {
-    const auto citron_build_version = "citron | 0.7.2";
+    const auto citron_build_version = "citron | 0.8.0";
 
     ui->setupUi(this);
     // Try and request the icon from Qt theme (Linux?)
--- src/citron/main.cpp
+++ src/citron/main.cpp
@@ -4749,7 +4749,7 @@ void GMainWindow::MigrateConfigFiles() {
 
 void GMainWindow::UpdateWindowTitle(std::string_view title_name, std::string_view title_version,
                                     std::string_view gpu_vendor) {
-    const auto window_title = "citron | 0.7.2";
+    const auto window_title = "citron | 0.8.0";
 
     if (title_name.empty()) {
         setWindowTitle(QString::fromStdString(window_title));
