6dbb6b37ce2d4662b4f9ab41fd8748466c615a83
CollectingW <ward9128@gmail.com>
2026-03-16 18:51:38 -0400

    Add Neo API for Nightly Releases

--- src/citron/updater/updater_service.cpp
+++ src/citron/updater/updater_service.cpp
@@ -44,7 +44,7 @@ namespace Updater {
 const std::string STABLE_UPDATE_URL =
     "https://git.citron-emu.org/api/v1/repos/Citron/Emulator/releases";
 const std::string NIGHTLY_UPDATE_URL =
-    "https://api.github.com/repos/Zephyron-Dev/Citron-CI/releases";
+    "https://api.github.com/repos/citron-neo/PR/releases";
 
 std::string ExtractCommitHash(const std::string& version_string) {
     // Hashes in git describe often start with 'g'.
